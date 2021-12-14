31eb2f070714a4bdb056173c986e531c5523cf0d
jeongyeon9254 <jeongyeon9254@naver.com>
2021-12-14 18:48:24 +0900

    [정연재] 헤더 분기 재설정

--- velog/src/components/ManiBar.js
+++ velog/src/components/ManiBar.js
@@ -1,5 +1,6 @@
 import React,{useState} from "react";
 import styled from "styled-components";
+import { withRouter } from 'react-router-dom'
 
 //컴포넌트
 import {Grid,IconButton,Text} from "../elements/ElementIndex";
@@ -28,20 +29,27 @@ const ManiBar = (props) => {
         history.push('/recent')
     }
 
+    if(props.location.pathname === '/' || props.location.pathname === '/recent') {
+        return(
+            <HeaderStyle>
+                <Grid is_flex height='64px' margin='1.5rem 0 0 0' position='relative'>
+                    <Grid _onClick={trend} opacity={trendOp} padding='10px' cursor='pointer' width='auto' is_flex>
+                        <IconButton height='22px' size='22px'></IconButton>
+                        <Text bold margin='0 0 0 10px'>트렌딩</Text>
+                    </Grid>
+                    <Grid _onClick={recent} opacity={recentOp} padding='10px' cursor='pointer' width='auto' is_flex margin='0 auto 0 15px'>
+                        <IconButton height='22px' clock size='22px'></IconButton>
+                        <Text bold margin='0 0 0 10px'>최신</Text>
+                    </Grid>
+                    <Grid width='114px' bottom='10px' left={left} position='absolute' borderBottom='2px solid #000'></Grid>
+                </Grid>
+            </HeaderStyle>
+        )
+    }
+
     return(
-        <HeaderStyle>
-            <Grid is_flex height='64px' margin='1.5rem 0 0 0' position='relative'>
-                <Grid _onClick={trend} opacity={trendOp} padding='10px' cursor='pointer' width='auto' is_flex>
-                    <IconButton height='22px' size='22px'></IconButton>
-                    <Text bold margin='0 0 0 10px'>트렌딩</Text>
-                </Grid>
-                <Grid _onClick={recent} opacity={recentOp} padding='10px' cursor='pointer' width='auto' is_flex margin='0 auto 0 15px'>
-                    <IconButton height='22px' clock size='22px'></IconButton>
-                    <Text bold margin='0 0 0 10px'>최신</Text>
-                </Grid>
-                <Grid width='114px' bottom='10px' left={left} position='absolute' borderBottom='2px solid #000'></Grid>
-            </Grid>
-        </HeaderStyle>
+       <>
+       </>
     )
 }
 
@@ -52,7 +60,8 @@ const HeaderStyle = styled.header`
     box-sizing:border-box;
     @media only screen and (max-width: 1730px) {
     padding :0 20px
+    z-index:9;
   }
 `
 
-export default ManiBar
+export default withRouter(ManiBar)
